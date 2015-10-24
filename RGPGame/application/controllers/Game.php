d7a3c7acee0b25c1c781f5057075214287693bc2
Rafael Siqueira <rafael@siqueira.net>
2015-10-23 23:43:08 -0200

    Coding of Initiative step

--- RGPGame/application/controllers/Game.php
+++ RGPGame/application/controllers/Game.php
@@ -21,6 +21,36 @@ class Game extends Custom_Controller {
         $this->render('game/start');
     }
 
+    public function initiative() {
+        $this->load->library('dice');
+        $this->load->library('session');
+
+        do {
+
+            $initiatives = array();
+
+            // loop through the players and determine the attack order
+            $players = $this->session->gamedata['players'];
+            foreach($players as $player) {
+
+                // keep the initiative number to use later
+                $initiative = $this->dice->next(range(1,20)) + $player->agility;
+                $initiatives[$initiative] = $player;
+            }
+
+        } while (sizeof($initiatives) == 1);
+
+        $sortedNumbers = array_keys($initiatives);
+
+        $first  = $initiatives[ max($sortedNumbers) ];
+        $second = $initiatives[ min($sortedNumbers) ];
+
+        $this->session->gamedata['players'] = array($first, $second);
+    }
+
+    public function attack() {
+    }
+
     private function prepareNewGame() {
 
         // load resources
